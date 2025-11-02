a0505c1219ba54da24e377d5ea6cf219b36240bc
deepalisahu2109 <deepalisahu2109@gmail.com>
2025-11-02 13:31:00 +0000

    conversion of datatype

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,24 @@
+let score=33
+console.log(typeof score)
+let b=String(score)
+console.log(typeof (typeof b))
+console.log(b)
+let scor="33bh"
+console.log(typeof scor)
+let a=Number(scor)
+console.log(typeof a)
+console.log(a)
+let num=1
+let booleanIsloggedIn=Boolean(num)
+console.log(num)
+console.log(typeof num)
+let D=undefined
+console.log(typeof D)
+console.log(D)
+let g=null
+console.log(typeof g)
+console.log(g)
+//"33"=33=convert in number
+//"33abc"=nan=convert in number
+//true=1,false=0
+//nan=not a number
